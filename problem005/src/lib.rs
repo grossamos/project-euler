8115a171b1b29ce4ca0e691c5b201254de48b643
grossamos <email@amosgross.com>
2022-07-21 09:55:43 +0200

    started work on problem 5

---
+++ problem005/src/lib.rs
@@ -0,0 +1,75 @@
+fn get_base(limit: u32) -> u32 {
+    let mut base = 2;
+    let mut prime_generator = Primes::new();
+
+    while prime_generator.get_recent_prime() < limit {
+        base *= prime_generator.get_recent_prime();
+        prime_generator.next();
+    }
+    base
+}
+
+fn test_divisiblity(base: u32, constant_divisor: u32, limit: u32) -> bool {
+    for divisor in 2..limit + 1 {
+        println!("{}", base / divisor);
+        if (base as f64 / divisor as f64) % constant_divisor as f64 != 0.0 {
+            return false;
+        }
+    }
+    true
+}
+
+struct Primes {
+    primes: Vec<u32>,
+    counter: u32,
+}
+
+impl Primes {
+    fn new() -> Primes {
+        Primes { primes: vec![2], counter: 2 }
+    }
+
+    fn get_recent_prime(&self) -> u32 {
+        self.primes[self.primes.len() - 1]
+    }
+}
+
+impl Iterator for Primes {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            self.counter += 1;
+            if !self.primes.iter().any(|x| self.counter % x == 0) {
+                self.primes.push(self.counter);
+                return Some(self.counter);
+            }
+        }
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Primes, get_base, test_divisiblity};
+
+    // even 
+    // factors: 2, 2, 3, 5, 7 ... (primes)
+    #[test]
+    fn gets_basic_increments() {
+        assert_eq!(get_base(10), 420);
+        assert_eq!(get_base(20), 19399380);
+    }
+
+    #[test]
+    fn generates_primes() {
+        let prime_generator = Primes::new();
+        assert_eq!(prime_generator.take(5).collect::<Vec<u32>>(), vec![3, 5, 7, 11, 13]);
+    }
+
+    #[test]
+    fn does_divisibility_test() {
+        assert!(test_divisiblity(2520, 1, 10));
+        assert!(!test_divisiblity(8888, 1, 10));
+    }
+}
