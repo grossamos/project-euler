78ad33cdb78253422792af862b3f21df671ef308
grossamos <email@amosgross.com>
2022-08-09 08:23:56 +0200

    Continued work on problem 14

--- problem014/src/lib.rs
+++ problem014/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn generate_next_collatz_item(base: u32) -> u32 {
+fn generate_next_collatz_item(base: u64) -> u64 {
     if base % 2 == 0 {
         base / 2
     } else {
@@ -8,18 +8,31 @@ fn generate_next_collatz_item(base: u32) -> u32 {
     }
 }
 
-fn get_length_of_chain(mut start: u32, known_chains: &mut HashMap<u32, usize>) -> usize {
+fn get_length_of_chain(mut start: u64, known_chains: &mut HashMap<u64, usize>) -> usize {
     let mut chain = vec![start];
-    while start != 1 || known_chains.contains_key(&start) {
+    while start != 1 && !known_chains.contains_key(&start) {
         start = generate_next_collatz_item(start);
         chain.push(start);
     }
     let starting_point = *known_chains.get(&start).unwrap_or(&1);
     for i in 0..chain.len() {
-        known_chains.insert(chain[i], i + starting_point);
+        known_chains.insert(chain[i], chain.len() - i - 1 + starting_point);
     }
-    println!("{:?}", chain);
     chain.len()
+}
+
+pub fn get_largest_sequence_until_limit(limit: u64) -> u64 {
+    let mut largest_seq = 0;
+    let mut seq_starter = 0;
+    let mut known_chains = HashMap::new();
+    for i in 2..limit + 1 {
+        let len = get_length_of_chain(i, &mut known_chains);
+        if largest_seq < len {
+            largest_seq = len;
+            seq_starter = i;
+        }
+    }
+    seq_starter
 }
 
 
@@ -27,7 +40,7 @@ fn get_length_of_chain(mut start: u32, known_chains: &mut HashMap<u32, usize>) -
 mod tests {
     use std::collections::HashMap;
 
-    use crate::{generate_next_collatz_item, get_length_of_chain};
+    use crate::{generate_next_collatz_item, get_length_of_chain, get_largest_sequence_until_limit};
 
     #[test]
     fn can_generate_next_collatz_item() {
@@ -45,10 +58,24 @@ mod tests {
     fn can_get_length_of_chain() {
         let mut known_chains = HashMap::new();
         let len = get_length_of_chain(13, &mut known_chains);
-        assert_eq!(len, 10);
+        assert_eq!(len, 10 as usize);
         assert_eq!(known_chains[&13], 10);
         assert_eq!(known_chains[&40], 9);
         assert_eq!(known_chains[&16], 5);
         assert_eq!(known_chains[&1], 1);
+
+        let mut known_chains = HashMap::new();
+        let len = get_length_of_chain(3, &mut known_chains);
+        assert_eq!(len, 8);
+
+        let mut known_chains = HashMap::new();
+        let len = get_length_of_chain(2, &mut known_chains);
+        assert_eq!(len, 2);
+    }
+
+    #[test]
+    fn can_get_longest_chain() {
+        assert_eq!(get_largest_sequence_until_limit(5), 3);
+        assert_eq!(get_largest_sequence_until_limit(3), 3);
     }
 }
--- problem014/src/main.rs
+++ problem014/src/main.rs
@@ -1,3 +1,5 @@
+use problem014::get_largest_sequence_until_limit;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_largest_sequence_until_limit(1000000));
 }
